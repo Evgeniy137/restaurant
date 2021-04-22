0a656aee8bcc9c03184f1ff91bed528dc269f50f
Evgeniy <vavilon11111111@gmail.com>
2021-04-22 09:27:48 +0300

    start create validation form

--- src/components/booking/_booking.js
+++ src/components/booking/_booking.js
@@ -20,12 +20,37 @@ let buttonClosePopup = document.querySelector(".booking-popup__close") // close 
 let popupContainer = document.querySelector(".booking-popup__key"); // container with add properties
 
 // open popup and ad property
-buttonOpenPopup.addEventListener("click", () => {
-    let elements = document.getElementById("form-booking").elements; // all elements with form
+buttonOpenPopup.addEventListener("click", (e) => {
+    e.preventDefault()
+    let elements = document.getElementById("form-booking").elements; // add elements with form
+
     for (let i = 0; i < elements.length; i++) {
         let item = elements.item(i);
         formData[item.name] = item.value;
     }
+
+
+
+
+    // form validate
+    let nameValidRegular = /аб*/g;
+    let gg = formData.Name;
+    let arr;
+    function valid() {
+        arr = nameValidRegular.exec(gg);
+        console.log(arr)
+    }
+    valid()
+    if (arr == null) {
+        console.log(arr)
+        return false;
+    }
+        
+    
+
+
+
+
 
     popupContainer.insertAdjacentHTML('afterbegin',
     `
@@ -34,7 +59,7 @@ buttonOpenPopup.addEventListener("click", () => {
         <li>${formData.Email}</li>
         <li>${formData.Phone}</li>
         <li>${formData.People}</li>
-        <li>${formData.Data}</li>
+        <li>${formData.Date}</li>
         <li>${formData.Time}</li>
     </ul>
     `
@@ -49,4 +74,5 @@ buttonClosePopup.addEventListener("click", () => {
     bookingPopupWrapper.style.display = "none";
     let delProperty = document.querySelector('.booking-popup__key--property').remove()
 })
-/* console.log(obj) */  /*  === / getting form data === */
+
+
