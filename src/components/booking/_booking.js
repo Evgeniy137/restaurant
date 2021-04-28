f06312ebce4fd741f524e5117a5a023b255f054b
Evgeniy <vavilon11111111@gmail.com>
2021-04-28 09:42:12 +0300

    modifer form

--- src/components/booking/_booking.js
+++ src/components/booking/_booking.js
@@ -1,39 +1,53 @@
-/* === library for date and time input === */
-const fp = flatpickr("#date", {
-
-    dateFormat: "Y-m-d"
-})
-const fpe = flatpickr("#time", {
-
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H:i",
-    time_24hr: true
-});
-/* === / library for date and time input === */
 
 /*  === getting form data === */
-let buttonOpenPopup = document.querySelector(".form__button--booking") // open popup
+let buttonOpenPopup = document.querySelector(".form__item-button") // open popup
 let bookingPopupWrapper = document.querySelector(".booking-popup") // overlay with popup
 let formData = {} // object with data form
 let buttonClosePopup = document.querySelector(".booking-popup__close") // close popup
 let popupContainer = document.querySelector(".booking-popup__key"); // container with add properties
 
+/* === library for date and time input === */
+const fp = flatpickr("#date", {
+    disableMobile: "true",
+    dateFormat: "Y-m-d",
+    allowInput: true,
+})
+const fpe = flatpickr("#time", {
+    disableMobile: "true",
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    allowInput: true,
+});
+/* === / library for date and time input === */
+
+
 // open popup and ad property
 buttonOpenPopup.addEventListener("click", (e) => {
     e.preventDefault()
-    let elements = document.getElementById("form-booking").elements; // add elements with form
 
+    let validElementsForm = document.getElementById("form-booking").reportValidity()
+    console.log(validElementsForm)
+    if (validElementsForm == false) {
+        return false;
+    }
+    let elements = document.getElementById("form-booking").elements; // add elements with form
+    
     for (let i = 0; i < elements.length; i++) {
         let item = elements.item(i);
+        
         formData[item.name] = item.value;
+
     }
 
 
 
 
     // form validate
-    let nameValidRegular = /аб*/g;
+    //const nameValidRegular = "/аб*/g";
+    /* const phineValidRegular = "1"
+    const mailValidRegular = "^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$";
     let gg = formData.Name;
     let arr;
     function valid() {
@@ -44,7 +58,7 @@ buttonOpenPopup.addEventListener("click", (e) => {
     if (arr == null) {
         console.log(arr)
         return false;
-    }
+    }  */
         
     
 
